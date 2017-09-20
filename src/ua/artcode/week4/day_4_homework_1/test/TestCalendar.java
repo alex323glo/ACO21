393f774f1edbf2136f9229e87ae6eb7a03ee0322
alex323glo <alex323glo@gmail.com>
2017-09-20 12:36:39 +0300

    I've started my practice on homework "Week4_Day5_homework".

---
+++ src/ua/artcode/week4/day_4_homework_1/test/TestCalendar.java
@@ -0,0 +1,9 @@
+package ua.artcode.week4.day_4_homework_1.test;
+
+public class TestCalendar {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/ua/artcode/week4/day_4_homework_1/test/TestEnum.java
@@ -0,0 +1,9 @@
+package ua.artcode.week4.day_4_homework_1.test;
+
+public class TestEnum {
+
+    public static void main(String[] args) {
+
+    }
+
+}
